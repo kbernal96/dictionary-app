03b99bb6f41bd3a8d0664a70bc0faab9f44b6d05
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-02-14 19:24:24 -0600

    created dictionary repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hallloooo!
+      </header>
+    </div>
+  );
+}
+
+export default App;
