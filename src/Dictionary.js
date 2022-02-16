aba1e2590f470f61f9da327c67cdc4820c086901
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-02-15 20:32:40 -0600

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event) {
         event.preventDefault();
         alert(`searching ${keyword}`);
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        
+        axios.get(url).then(handleResponse);
     }
 
     function handleKeyword(event) {
