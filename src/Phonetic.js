ccfcd1511a44c4e543859c5bc5a759ff9e8dcd0a
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-02-17 20:35:42 -0600

    added rel attribute

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetic (props) {
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">🔊</a>
+            <a href={props.phonetic.audio} target="_blank" rel="noopener">🔊</a>
             <br/>
             {props.phonetic.text}
         </div>
