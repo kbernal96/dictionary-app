2f097e6a01c4d81f1acb9ba4b82ae5638a89667a
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-02-17 20:31:56 -0600

    have added the phonetic; need to refined

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
    if (props.results) {
        return (
        <div className="Results">
            <h2 className="text-capitalize">{props.results.word}</h2>
+
+            {props.results.phonetics.map(function(phonetic, index) {
+                return (
+                    <div key={index}>
+                        <Phonetic phonetic={phonetic} />
+                    </div>
+                );
+            })}
 
            {/*looping through object passed from props.results.word*/}
 
