6f678a19ae45f0ec56f216545a4ceba3f75377ee
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-02-20 23:00:14 -0600

    added a footer element and styled it

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         <img src={logo} className="logo" alt="logo"/>
         <Dictionary defaultKeyword="flibbertigibbet"/>
       </div>
+      <footer mt-5>
+        Open-source code by 
+        <a className="coder" href="https://github.com/kbernal96/dictionary-app" target="_blank" rel="noreferrer"> Karla Bernal</a>
+      </footer>
+
     </div>
   );
 }
